26980bed0c7da61e96a50265b0ad50413f8722db
juortega5 <juortega5@poligran.edu.co>
2020-04-05 12:54:14 -0500

    prueba carga

--- resources/views/user/from_usuario.blade.php
+++ resources/views/user/from_usuario.blade.php
@@ -1,28 +1,28 @@
 	<div class="form-group">
 		<label for="">Nombre</label>
-		<input type="text" name="nombre" class="form-control">
+		<input type="text" @isset($user->nombre) value="{{ $user->nombre }}" @endisset name="nombre" class="form-control">
 	</div> 
 	<div class="form-group">
 		<label for="">Apellidos</label>
-		<input type="text" name="apellidos" class="form-control">
+		<input type="text" @isset($user->apellidos) value="{{ $user->apellidos }}" @endisset name="apellidos" class="form-control">
 	</div> 
 	<div class="form-group">
 		<label for=""># Celular</label>
-		<input type="text" name="celular" class="form-control">
+		<input type="text" @isset($user->celular) value="{{ $user->celular }}" @endisset name="celular" class="form-control">
 	</div> 
 	<div class="form-group">
 		<label for="">Direcion</label>
-		<input type="text" name="direcion" class="form-control">
+		<input type="text" @isset($user->direcion) value="{{ $user->direcion }}" @endisset name="direcion" class="form-control">
 	</div> 
 	<div class="form-group">
 		<label for="">Monto deuda</label>
-		<input type="number" name="monto_deuda" class="form-control">
+		<input type="number" @isset($user->monto_deuda) value="{{ $user->monto_deuda }}" @endisset name="monto_deuda" class="form-control">
 	</div> 
 	<div class="form-group">
 		<label for="">slug</label>
-		<input type="number" name="slug" class="form-control">
+		<input type="number" @isset($user->slug) value="{{ $user->slug }}" @endisset name="slug" class="form-control">
 	</div> 
 	<div class="form-group">
 		<label for="">imagen</label>
-		<input type="file" name="imagenusu" >
+		<input type="file" @isset($user->imagenusu) value="{{ $user->imagenusu }}" @endisset name="imagenusu" >
 	</div>
